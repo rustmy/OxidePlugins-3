5872fbb7b8bdaada9ac6a3ab1551294bbd2875dd
Joshua Dassinger <joshman2934@gmail.com>
2016-10-09 22:33:46 -0500

    AutoCodeLock - Fixed Plugin Info

--- OxidePlugins/OxidePlugins/AutoCodeLock/AutoCodelock.cs
+++ OxidePlugins/OxidePlugins/AutoCodeLock/AutoCodelock.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace Oxide.Plugins
 {
     // ReSharper disable once UnusedMember.Global
-    [Info("MJSU", "MJSU", "0.0.1")]
+    [Info("AutoCodeLock", "MJSU", "0.0.1")]
     [Description("Auto adds a codelock to a placed door and set the code")]
     class AutoCodeLock : RustPlugin
     {
